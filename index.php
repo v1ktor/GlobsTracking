5879df5082521908bbfb37bbd86925c91d896052
Viktor Kalinin <kalinin.viktor@gmail.com>
2015-11-17 14:03:01 +0200

    implement basic helper class with methods for displaying various messages

--- index.php
+++ index.php
@@ -10,8 +10,10 @@ require_once 'src/Globs/Jira/Api/Client/CurlClient.php';
 require_once 'src/Globs/Jira/Api/Client/CredentialsInterface.php';
 require_once 'src/Globs/Jira/Api/Client/Credentials.php';
 require_once 'src/Globs/Jira/Jira.php';
+require_once 'src/Globs/Helper.php';
 
 $jira = new v1\Globs\Jira\Jira($endpoint, new \v1\Globs\Jira\Api\Client\Credentials($username, $password));
+$helper = new v1\Globs\Helper();
 
 $jql = "asdasdasdproject = AIW16 AND issuetype = \"Software Defect\" AND status in (\"Open - On Hold\", \"Open - Glob\") AND \"US DTS ID\" is not EMPTY";
 
@@ -21,7 +23,7 @@ echo "<br><br>";
 
 if (isset($data["errorMessages"])) {
     foreach ($data["errorMessages"] as $errorMessage) {
-        echo "<div>" . $errorMessage . "</div>";
+        echo $helper->displayErrorMessage($errorMessage);
     }
 } else {
     foreach ($data as $issue) {
---
+++ src/Globs/Helper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace v1\Globs;
+
+class Helper
+{
+    public function __construct()
+    {
+    }
+
+    public function displayErrorMessage($message)
+    {
+        return "<div style=\"padding: 15px; margin-bottom: 20px; border: 1px solid #ebccd1;
+border-radius: 4px; color: #a94442; background-color: #f2dede;\">" . $message . "</div>";
+    }
+
+    public function displaySuccessMessage($message)
+    {
+        return "<div style=\"padding: 15px; margin-bottom: 20px; border: 1px solid #d6e9c6;
+border-radius: 4px; color: #3c763d; background-color: #dff0d8;\">" . $message . "</div>";
+    }
+
+    public function displayInfoMessage($message)
+    {
+        return "<div style=\"padding: 15px; margin-bottom: 20px; border: 1px solid #bce8f1;
+border-radius: 4px; color: #31708f; background-color: #d9edf7;\">" . $message . "</div>";
+    }
+
+    public function displayWarningMessage($message)
+    {
+        return "<div style=\"padding: 15px; margin-bottom: 20px; border: 1px solid #faebcc;
+border-radius: 4px; color: #8a6d3b; background-color: #fcf8e3;\">" . $message . "</div>";
+    }
+}
